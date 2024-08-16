301afe87257d6ef8db93260ff7261c06d8021f69
EGOproject <enockmci@gmail.com>
2024-08-16 12:17:35 +0300

    navbar and login or sign up batch 1 Backup by: EGoproject on Fri Aug 16 12:17:35 EAT 2024

--- ginger/core/views.py
+++ ginger/core/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def login(request):
+    return render(request, 'core/login.html')
--- ginger/ginger/urls.py
+++ ginger/ginger/urls.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
-from core.views import index
+from core.views import index, contact, login
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', index, name="index"),
+    path('contact/', contact, name="contact"),
+    path('login/', login, name="login"),
+    path('admin/', admin.site.urls),
     #path('core/', include('core.urls')),
 ]

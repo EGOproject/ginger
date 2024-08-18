e21e643c734471fbe0d4eb38f99e90fd71644532
EGOproject <enockmci@gmail.com>
2024-08-18 03:10:46 +0300

    product tables and products Backup by: EGoproject on Sun Aug 18 03:10:46 EAT 2024

--- ginger/items/models.py
+++ ginger/items/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -13,5 +14,16 @@ class Category(models.Model):
 
 
 class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
+    item_img1 = models.ImageField(upload_to='item_images', blank=True, null=True)
+    item_img2 = models.ImageField(upload_to='item_images', blank=True, null=True)
+    item_img3 = models.ImageField(upload_to='item_images', blank=True, null=True)
+    available = models.BooleanField(default=True)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name        # return actual names of fields in table
